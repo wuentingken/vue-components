00633281c7882d853df7925b6a351bdff23ae94d
wuentingken <377954252@qq.com>
2021-06-14 12:27:13 +0800

    feat: 初始化

---
+++ packages/button/src/button.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <Button @click="this.add">按钮</Button>
+    </div>
+</template>
+<script>
+
+export default{
+    data() {
+        return {
+            count: 1
+        }
+    },
+    methods: {
+        add() {
+            this.count++
+        }
+    }
+}
+</script>
---
+++ packages/step/src/step.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div>{{count}}</div>
+        <Button @click="this.add">下一步</Button>
+    </div>
+</template>
+<script>
+
+export default{
+    data() {
+        return {
+            count: 1
+        }
+    },
+    methods: {
+        add() {
+            this.count++
+        }
+    }
+}
+</script>
